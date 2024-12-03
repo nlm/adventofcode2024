83c410a1ef5f7f5e1f107406ed5aa0a33d23fd4b
Nicolas Limage <nlimage@scaleway.com>
2024-12-03 06:43:08 +0100

    day 03

---
+++ 03/stage.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"regexp"
+	"slices"
+
+	"github.com/nlm/adventofcode2024/internal/iterators"
+	"github.com/nlm/adventofcode2024/internal/stage"
+	"github.com/nlm/adventofcode2024/internal/utils"
+)
+
+func Stage1(input io.Reader) (any, error) {
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	total := 0
+	for _, line := range slices.Collect(iterators.MustLines(input)) {
+		for _, match := range re.FindAllString(line, -1) {
+			stage.Println("match:", match)
+			n := iterators.Map(re.FindStringSubmatch(match)[1:], utils.MustAtoi)
+			total += n[0] * n[1]
+		}
+	}
+	return total, nil
+}
+
+func Stage2(input io.Reader) (any, error) {
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	total := 0
+	enabled := true
+	for _, line := range slices.Collect(iterators.MustLines(input)) {
+		for _, match := range re.FindAllString(line, -1) {
+			stage.Println("match:", match)
+			switch match {
+			case `do()`:
+				enabled = true
+			case `don't()`:
+				enabled = false
+			default:
+				n := iterators.Map(re.FindStringSubmatch(match)[1:], utils.MustAtoi)
+				if enabled {
+					total += n[0] * n[1]
+				}
+			}
+		}
+	}
+	return total, nil
+}
