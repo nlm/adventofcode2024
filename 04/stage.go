8c13ebff500b786373a9c9df9b610363a5b66b8d
Nicolas Limage <nlimage@scaleway.com>
2024-12-06 08:06:46 +0100

    day 04 enhancements

--- 04/stage.go
+++ 04/stage.go
@@ -8,55 +8,59 @@ import (
 	"github.com/nlm/adventofcode2024/internal/utils"
 )
 
-func SearchXmas(m *matrix.Matrix[byte], orig matrix.Coord, dir matrix.Vec) int {
+func SearchXmas(m *matrix.Matrix[byte], orig matrix.Coord, dir matrix.Vec) bool {
 	word := []byte{'X', 'M', 'A', 'S'}
 	for i := range len(word) {
 		curr := orig.Add(dir.Mul(i))
 		if !m.InCoord(curr) {
 			// stage.Println("out of bounds")
-			return 0
+			return false
 		}
 		if m.AtCoord(curr) != word[i] {
-			return 0
+			return false
 		}
 	}
 	stage.Println("found", orig, "->", dir)
-	return 1
+	return true
 }
 
 func Stage1(input io.Reader) (any, error) {
 	total := 0
 	m := utils.Must(matrix.NewFromReader(input))
 	for coord := range m.IterCoords() {
+		// XMAS always starts with an X
 		if m.AtCoord(coord) != 'X' {
 			continue
 		}
 		stage.Println("Search", coord)
+		// Lookup XMAS in every direction
 		for _, vec := range []matrix.Vec{
-			// Clock 12
+			// Clock 12:00
 			matrix.Up,
-			// Clock 1.5
+			// Clock 1:30
 			matrix.UpRight,
-			// Clock 3
+			// Clock 3:00
 			matrix.Right,
-			// Clock 4.5
+			// Clock 4:30
 			matrix.DownRight,
-			// CLock 6
+			// Clock 6:00
 			matrix.Down,
-			// Clock 7.5
+			// Clock 7:30
 			matrix.DownLeft,
-			// Clock 9
+			// Clock 9:00
 			matrix.Left,
-			// Clock 10.5
+			// Clock 10:30
 			matrix.UpLeft,
 		} {
-			total += SearchXmas(m, coord, vec)
+			if SearchXmas(m, coord, vec) {
+				total++
+			}
 		}
 	}
 	return total, nil
 }
 
-func SearchCrossMass(m *matrix.Matrix[byte], orig matrix.Coord) int {
+func SearchCrossMass(m *matrix.Matrix[byte], orig matrix.Coord) bool {
 	vecs := []matrix.Vec{
 		matrix.UpLeft,
 		matrix.UpRight,
@@ -67,30 +71,34 @@ func SearchCrossMass(m *matrix.Matrix[byte], orig matrix.Coord) int {
 	for _, v := range vecs {
 		curr := orig.Add(v)
 		if !m.InCoord(curr) {
-			return 0
+			return false
 		}
 		xs[m.AtCoord(curr)]++
 	}
-	// MAS are there
+	// check that the two M & S are there
 	if xs['M'] != 2 || xs['S'] != 2 {
-		return 0
+		return false
 	}
-	// MAM / SAS
+	// check that it's not MAM / SAS
 	if m.AtCoord(orig.Add(matrix.UpLeft)) == m.AtCoord(orig.Add(matrix.DownRight)) {
-		return 0
+		return false
 	}
-	return 1
+	return true
 }
 
 func Stage2(input io.Reader) (any, error) {
 	total := 0
 	m := utils.Must(matrix.NewFromReader(input))
 	for coord := range m.IterCoords() {
+		// We want to look around A
 		if m.AtCoord(coord) != 'A' {
 			continue
 		}
 		stage.Println("Search", coord)
-		total += SearchCrossMass(m, coord)
+		// Look around this A for matches
+		if SearchCrossMass(m, coord) {
+			total++
+		}
 	}
 	return total, nil
 }
